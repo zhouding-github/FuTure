37d3521e64a5db95e2d8b730b95c811def607428
zhouding-github <17683913028@163.com>
2020-06-10 20:55:39 +0800

    修改文件

---
+++ 接口测试/BM预算系统/WebApiBM/test.py
@@ -0,0 +1,20 @@
+'''
+[['合计'，'M1=52','M5=65'],['工资'，'M1=56','M2=20','M4=50']]
+
+{{
+ code：hj,
+ Month1:52,
+ Month5:65
+},
+{
+ code：gz,
+ Month1:56
+ ,
+ Month2:20,
+ Month4：50
+}
+}
+'''
+import  接口测试.BM预算系统.WebApiBM.ApiBM as bm
+st = [['合计', 'M1=52', 'M5=65'], ['工资', 'M1=56', 'M2=20', 'M4=50']]
+print(len(st))
